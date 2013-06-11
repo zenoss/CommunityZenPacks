5fb09a5026f59234b82a1446659333ccfa652a2e
Daniel Robbins <drobbins@funtoo.org>
2013-06-11 17:14:48 +0000

    new ng stuff for updated zenpack auto-builder

---
+++ scripts/zpbuild_globals_ng.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+import os
+
+build_targets = ( 
+	"Zenoss Core 3.2.x" ,
+	"Zenoss Resource Manager 4.1.x" ,
+	"Zenoss Core 4.2.x" 
+)
+
+python_targets = {
+	"Zenoss Core 3.2.x" : "2.6",
+	"Zenoss Resource Manager 4.1.x" : "2.7",
+	"Zenoss Core 4.2.x" : "2.7"
+}
+
+zprepo = os.path.join(os.path.expanduser("~"), "zprepo")
+zptemp = "/var/tmp/zpbuild-ng"
+zpconf = zptemp + "/etc"
+zpdef_file = zpconf + "/zpdefs.json"
+reldef_file = zpconf + "/reldefs.json"
+builddefs = zpconf + "/builddefs.xml"
+if not os.path.exists(zpconf):
+	os.makedirs(zpconf)
