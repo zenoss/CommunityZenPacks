197e98cd1ab6d7c32bad40cef8486ffb9629d804
Daniel Robbins <drobbins@funtoo.org>
2013-06-21 01:51:20 +0000

    latest JSON stuff, cron fixes, semantic mediawiki/approvedrevs bogus query detection

--- scripts/zpcron.sh
+++ scripts/zpcron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mydir=$(dirname $(readlink -f $0))
 cd $mydir
-$mydir/zpparse && $mydir/zpbuild 2>&1 | tee ~/zprepo/zpbuild.txt
+$mydir/zpparse-ng && $mydir/zpbuild-ng 2>&1 | tee ~/zprepo-ng/zpbuild.txt
